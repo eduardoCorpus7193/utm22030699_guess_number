9aa7dc7f604347fb8487aa18f40065792166ead3
Jaime Ibarra <utm22030687@gmail.com>
2023-06-06 09:21:29 -0600

    feat: while,range and nameTwo

--- multiplayer_guess_number.py
+++ multiplayer_guess_number.py
@@ -2,3 +2,9 @@ import getpass #It is the library for hiding characters
 plyo = input("What is your name: ")#Name of player one 
 num_guess = int(getpass.getpass(plyo+", enter the number to be guessed: "))#we send to call to the library characteristics 
 
+while True: #"While" loops to chose a number
+    if num_guess>0 and num_guess<16:#Range to chose a number 
+        break
+    else:
+        print ("The number is out of range")#Invalid number
+plyt = input("Your name?: ") #Name of player two
