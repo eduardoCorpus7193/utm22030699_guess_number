ffe69ac85795d44ab5c8aab48e021a336f1e6ae9
Jaime Ibarra <utm22030687@gmail.com>
2023-06-06 08:50:35 -0600

    feat: new document

---
+++ multiplayer_guess_number.py
@@ -0,0 +1,4 @@
+import getpass #It is the library for hiding characters 
+plyo = input("What is your name: ")#Name of player one 
+num_guess = int(getpass.getpass(plyo+", enter the number to be guessed: "))#we send to call to the library characteristics 
+
