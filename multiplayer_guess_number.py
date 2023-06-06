70489bad9c81346674660010ce203253f86f31f2
Jaime Ibarra <utm22030687@gmail.com>
2023-06-06 10:57:58 -0600

    fix: first bug

--- multiplayer_guess_number.py
+++ multiplayer_guess_number.py
@@ -12,11 +12,13 @@ plyt = input("Your name?: ") #Name of player two
 score = 100  # Starting score for player 2
 while score > 0: 
     guess = int(input(plyt+", try to guess the number: "))#Player 2 needs to guess the secret number 
-     if (guess == num_guess) : #Comparision with the secret number
+    
+    if (guess == num_guess) : #Comparision with the secret number
         print("Congratulations! You guessed the number correctly.")#message (if equal to the secret number)
         break
     else:
         score -= 10#If the answer is incorrect, subtract points
         print(f"Wrong guess! Your score is now " + str(score))#If the answer is incorrect, show the message
+
 if score == 0: #comparision 
     print(plyo +"wins! ,", + plyt ," score reached 0.")# #if player 2 losse all points.
